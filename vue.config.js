dd71ec7e73f4a628e259b948bbd5ca6b04053e41
Tony Trinh <tony19@gmail.com>
2021-09-30 18:09:11 -0500

    serve br compressed .js files

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,26 @@
+const CompressionPlugin = require("compression-webpack-plugin")
+
 module.exports = {
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new CompressionPlugin({
+        filename: "[path][base].br",
+        algorithm: "brotliCompress",
+        test: /\.js$/,
+      })
+    ]
+  },
+  devServer: {
+    onBeforeSetupMiddleware(server) {
+      server.app.use('*.js', (req, res, next) => {
+        if (req.get('Accept-Encoding').includes('br')) {
+          req.url += '.br'
+          res.set('Content-Encoding', 'br')
+          res.set('Content-Type', 'application/javascript; charset=utf-8')
+        }
+        next()
+      })
+    }
+  }
 }
